755555919a6262f6f82517651a30cf9cfdeee24d
Pedro-Vargas26 <backupofpedros@gmail.com>
2025-11-20 10:14:24 -0800

    Refactor DynamicArray class and remove binary search

--- DynamicArray.h
+++ DynamicArray.h
@@ -1,66 +1,49 @@
-
 #pragma once
-#ifndef DYNAMICARRAY_H
-#define DYNAMICARRAY_H
-
 #include <cstddef>
 #include <exception>
+#include <iostream>
 
 template<typename T>
-int binary_search(const T* haystack, std::size_t arrSize, const T& needle) noexcept;
-
-template<typename T>
-class DynamicArray {
+class DynamicArray
+{
 private:
     T* p_ptr;
-    std::size_t arrSize;
-    std::size_t arrCapacity;
+    int  arrSize;
+    int  arrCapacity;
     bool isSorted;
+
 public:
+ 
     DynamicArray();
-    DynamicArray(const DynamicArray&);
+    DynamicArray(const DynamicArray& other);
+    DynamicArray& operator=(const DynamicArray& other);
     ~DynamicArray();
 
-    const T* data() const noexcept;
 
-    void append(const T&) noexcept;
-    void sorted_insert(const T&) noexcept;
-    bool exists(const T&) const noexcept;
-    bool empty() const noexcept;
-    bool sorted()const noexcept;
-
-    int remove(const T&) noexcept;
-    int remove(const T&, bool) noexcept;
+    void append(const T& obj) noexcept;
     void clear() noexcept;
-    void sort();
+    int  find(const T& needle) const noexcept;
+    bool exists(const T& needle) const noexcept;
+    bool empty() const noexcept;
+    size_t size() const noexcept;
+    size_t capacity() const noexcept;
 
-    std::size_t size() const noexcept;
-    std::size_t capacity() const noexcept;
-    int find(const T&) const noexcept;
+
+    void sort();
+    bool sorted() const noexcept;
+    void sorted_insert(const T& obj) noexcept;
+
+
+    int remove(const T& needle) noexcept;
+    int remove(const T& needle, bool allOrOne) noexcept;
+
 
     T& operator[](int index);
     const T& operator[](int index) const;
 
-    friend int binary_search<T>(const T*, size_t, const T&) noexcept;
+
+    const T* data() const noexcept;
 };
 
-template <typename T>
-int binary_search(const T* haystack, size_t arrSize, const T& needle) noexcept {
-    if (!haystack || arrSize == 0)
-        return 0;
 
-    std::size_t low = 0;
-    std::size_t high = arrSize;
-
-    while (low < high) {
-        std::size_t mid = (low + high) / 2;
-        if (haystack[mid] < needle)
-            low = mid + 1;
-        else
-            high = mid;
-    }
-
-    return static_cast<int>(low);
-}
-
-#endif
+#include "DynamicArray.cpp"
