1ca67d9356db131d796a86f81d35af40e1e8e87a
Anthony1418 <anthonyherrera2314@gmail.com>
2025-11-23 22:21:24 -0800

    Fixed sub menu options

--- main.cpp
+++ main.cpp
@@ -12,29 +12,38 @@ using namespace std;
 void option1();
 void option2();
 void option3();
-void mvStr(char*,const int, string&);
+void mvStr(char*, const int, string&);
 HashTable<Student> translateFile(const string&, int&);
 
 
 char mainMenu();
 
-int main(){
-    switch (mainMenu()) {
-    case '1':
-        option1();
-        break;
-    case '2':
-        option2();
-        break;
-    case '3':
-        option3();
-        break;
-   }
+int main() {
+    bool running = true;
+
+    while (running) {
+        system("cls");
+        switch (mainMenu()) {
+        case '1':
+            option1();
+            break;
+        case '2':
+            option2();
+            break;
+        case '3':
+            option3();
+            break;
+        case '0':
+            running = false;   // Exit cleanly
+            break;
+        }
+    }
 }
 
+
 void option1() {
-    char choice = 'A';
-    bool running = true;
+    //char choice = 'A';
+    //bool running = true;
     do {
         system("cls");
 
@@ -48,26 +57,29 @@ void option1() {
         cout << "\t" << string(85, (char)196) << "\n";
         cout << "\t\t" << "0 > return\n";
         cout << "\t" << string(85, (char)205) << "\n";
-        while ((choice < '0') || (choice > '3')) {
-            choice = inputChar("\tERROR INVALID CHOICE. TRY AGAIN:");
-        }
-        switch (choice) {
-        case '1':
+
+            //choice = inputChar("\tERROR INVALID CHOICE. TRY AGAIN:");
+        
+        switch (inputInteger("\tOption: ", 0, 5))
+        {
+        case 1:
             break;
-        case '2':
+        case 2:
             break;
-        case '3':
+        case 3:
             break;
-        case '4':
+        case 4:
             break;
-        case '5':
+        case 5:
             break;
-        case '0':
-            running = false;
-            break;
+        case 0: return;
+        default:
+        {
+            cout << "\n\tERROR: invalid input...";
+        }break;
         }
         system("pause");
-    } while (running);
+    } while (true);
 }
 void option2() {
     char choice = 'A';
@@ -198,94 +210,94 @@ void option3() {
 
 
 
-    do{
+    do {
         system("cls");
-    cout << "\t" << "3 > Application using hashing\n";
-    cout << "\t" << string(80, (char)205) << "\n";
-    cout << "\t\t" << "A > Read data file, hash and insert into the dynamic array\n";
-    cout << "\t\t" << "B > Search an element from the dynamic array\n";
-    cout << "\t\t" << "C > Insert an element into the dynamic array\n";
-    cout << "\t\t" << "D > Remove an element from the dynamic array\n";
-    cout << "\t\t" << "E > Display all records from the array\n";
-    cout << "\t" << string(80, (char)196) << "\n";
-    cout << "\t\t" << "0  > return\n";
-    cout << "\t" << string(80, (char)205) << "\n";
-    choice = toupper(inputChar("\t\tOption :"));
-    switch (choice) {
-    case 'A':
-        n = inputInteger("\t\tEnter a number of read-in records: ", true);
-        try {
-            haystack = translateFile(fileName, n);
-            cout << "\t\t" << n << " records have been inserted. \n\n";
-        }
-        catch (...) {
-            cerr << "\t\t" << "ERROR: FILE OPERATION FAILED. \n\n";
-        }
-
-        break;
-    case 'B':
-        needle = inputInteger("\t\tEnter a student ID to search:", true);
-        if (haystack.search(needle)) {
-            cout << "found. ";
-        }
-        else {
-            cout << "\t\t" << "ERROR: ID not found. \n\n";
-        }
-        break;
-    case 'C':
-        tempInt = inputInteger("\t\tEnter a new student ID:   ", true);
-        do {
+        cout << "\t" << "3 > Application using hashing\n";
+        cout << "\t" << string(80, (char)205) << "\n";
+        cout << "\t\t" << "A > Read data file, hash and insert into the dynamic array\n";
+        cout << "\t\t" << "B > Search an element from the dynamic array\n";
+        cout << "\t\t" << "C > Insert an element into the dynamic array\n";
+        cout << "\t\t" << "D > Remove an element from the dynamic array\n";
+        cout << "\t\t" << "E > Display all records from the array\n";
+        cout << "\t" << string(80, (char)196) << "\n";
+        cout << "\t\t" << "0  > return\n";
+        cout << "\t" << string(80, (char)205) << "\n";
+        choice = toupper(inputChar("\t\tOption :"));
+        switch (choice) {
+        case 'A':
+            n = inputInteger("\t\tEnter a number of read-in records: ", true);
             try {
-                tempStr = inputString("\t\tEnter the student's name:  ", true);
-                mvStr(studentName, BUFFER_SIZE, tempStr);
-                errorMarked = false;
+                haystack = translateFile(fileName, n);
+                cout << "\t\t" << n << " records have been inserted. \n\n";
             }
-            catch (const std::runtime_error& e) {
-                cerr << "\t\t" << e.what() << "\n";
+            catch (...) {
+                cerr << "\t\t" << "ERROR: FILE OPERATION FAILED. \n\n";
             }
-        } while (errorMarked);
-        errorMarked = true;
-        do {
-            try {
-                tempStr = inputString("\t\tEnter the student's major: ", true);
-                mvStr(studentMajor, BUFFER_SIZE, tempStr);
-                errorMarked = false;
-            }
-            catch (const std::runtime_error& e) {
-                cerr << "\t\t" << e.what() << "\n";
-            }
-        } while (errorMarked);
-       
-        tempDouble = inputDouble("\t\tEnter a student's GPA(1.0...4.0):", 1.0, 4.0);
 
-        cout << "\t\t" << "Inserted the new record. \n\n";
-        break;
-    case 'D':
-        needle = inputInteger("\t\tEnter a student ID to remove: ", true);
+            break;
+        case 'B':
+            needle = inputInteger("\t\tEnter a student ID to search:", true);
+            if (haystack.search(needle)) {
+                cout << "found. ";
+            }
+            else {
+                cout << "\t\t" << "ERROR: ID not found. \n\n";
+            }
+            break;
+        case 'C':
+            tempInt = inputInteger("\t\tEnter a new student ID:   ", true);
+            do {
+                try {
+                    tempStr = inputString("\t\tEnter the student's name:  ", true);
+                    mvStr(studentName, BUFFER_SIZE, tempStr);
+                    errorMarked = false;
+                }
+                catch (const std::runtime_error& e) {
+                    cerr << "\t\t" << e.what() << "\n";
+                }
+            } while (errorMarked);
+            errorMarked = true;
+            do {
+                try {
+                    tempStr = inputString("\t\tEnter the student's major: ", true);
+                    mvStr(studentMajor, BUFFER_SIZE, tempStr);
+                    errorMarked = false;
+                }
+                catch (const std::runtime_error& e) {
+                    cerr << "\t\t" << e.what() << "\n";
+                }
+            } while (errorMarked);
 
-        idx = haystack.remove(needle);
-        cout << "\t\t";
-        if (idx != -1) {
-            cout << "Student record index #" << idx << " with ID: " << needle << " has been removed.";
+            tempDouble = inputDouble("\t\tEnter a student's GPA(1.0...4.0):", 1.0, 4.0);
+
+            cout << "\t\t" << "Inserted the new record. \n\n";
+            break;
+        case 'D':
+            needle = inputInteger("\t\tEnter a student ID to remove: ", true);
+
+            idx = haystack.remove(needle);
+            cout << "\t\t";
+            if (idx != -1) {
+                cout << "Student record index #" << idx << " with ID: " << needle << " has been removed.";
+            }
+            else {
+                cout << "ERROR: ID CANNOT BE FOUND.";
+            }
+            cout << "\n\n";
+            //erase logic here
+            //success: student record index#n with ID: idnum has been removed. 
+            //failed:  ERROR: ID cannot be found.
+
+            break;
+        case 'E':
+            if (haystack.empty())cout << "\n\n\t\t" << "ERROR: no record found.\n\n\t";
+            else
+                for (int i = 0; i < haystack.size(); i++)
+                    cout << "[" << i << "]: -> " << haystack[i] << "\n";
+            break;
+        default:
+            running = false;
         }
-        else {
-            cout << "ERROR: ID CANNOT BE FOUND.";
-        }
-        cout << "\n\n";
-        //erase logic here
-        //success: student record index#n with ID: idnum has been removed. 
-        //failed:  ERROR: ID cannot be found.
-
-        break;
-    case 'E':
-        if (haystack.empty())cout << "\n\n\t\t" << "ERROR: no record found.\n\n\t";
-        else
-            for (int i = 0; i < haystack.size();i++) 
-                cout << "[" << i << "]: -> " << haystack[i] << "\n";
-        break;
-    default:
-        running = false;
-    }
-    system("pause");
-}while (running);
+        system("pause");
+    } while (running);
 }
