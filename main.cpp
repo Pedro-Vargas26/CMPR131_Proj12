27a9ab8643c18d5dda0d34f001c0f4d4fa2b2366
Pedro-Vargas26 <backupofpedros@gmail.com>
2025-11-23 22:57:32 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,7 +14,9 @@ void option2();
 void option3();
 void mvStr(char*, const int, string&);
 HashTable<Student> translateFile(const string&, int&);
-
+void populate(DynamicArray<string>&, int);
+int serialStringSearch(DynamicArray<string>& , int , const string& );
+int binaryStringSearch(DynamicArray<string>&, int,int, const string&);
 
 char mainMenu();
 
@@ -34,48 +36,144 @@ int main() {
             option3();
             break;
         case '0':
-            running = false;   // Exit cleanly
+            running = false;
             break;
         }
     }
 }
 
 
+void populate(DynamicArray<string>& dataset, int n) {
+    for (int i = 0; i < n; i++) {
+
+        int randLength = 5 + rand() % 20; 
+        string s = "";
+
+        for (int j = 0; j < randLength; j++) {
+            s += 'a' + rand() % 26;  
+        }
+
+        dataset.append(s);
+    }
+}
+
+int serialStringSearch(DynamicArray<string>& ds, int index, const string& needle) {
+    if (index >= ds.size())
+        return -1;
+    if (ds[index] == needle)
+        return index;
+    return serialStringSearch(ds, index + 1, needle);
+}
+int binaryStringSearch(DynamicArray<string>& ds, int left, int right, const string& needle) {
+    if (left > right)
+        return -1;
+
+    int mid = (left + right) / 2;
+
+    if (ds[mid] == needle)
+        return mid;
+
+    if (needle < ds[mid])
+        return binaryStringSearch(ds, left, mid - 1, needle);
+    else
+        return binaryStringSearch(ds, mid + 1, right, needle);
+}
+
 void option1() {
-    //char choice = 'A';
-    //bool running = true;
+    int n = 0;
+    int idx = 0;
+    char choice = 'A';
+    DynamicArray<string> ds;
+    string tempStr = "";
     do {
         system("cls");
 
         cout << "\t1 > Searching unsorted dynamic arrays\n";
         cout << "\t" << string(85, (char)196) << "\n";
-        cout << "\t\t" << "A > Read data file and store into the array\n";
-        cout << "\t\t" << "B > Add an element to the dynamic array\n";
-        cout << "\t\t" << "C > Display elements from the array\n";
-        cout << "\t\t" << "D > Search for an element from the array\n";
-        cout << "\t\t" << "E > Clear the array\n";
+        cout << "\t\t" << "1 > Read data file and store into the array\n";
+        cout << "\t\t" << "2 > Add an element to the dynamic array\n";
+        cout << "\t\t" << "3 > Display elements from the array\n";
+        cout << "\t\t" << "4 > Search for an element from the array\n";
+        cout << "\t\t" << "5 > Clear the array\n";
         cout << "\t" << string(85, (char)196) << "\n";
         cout << "\t\t" << "0 > return\n";
         cout << "\t" << string(85, (char)205) << "\n";
-        
-        switch (toupper(inputChar("\t\tOption: ", static_cast<string>("abcdefgh0"))))
+
+        switch (inputInteger("\tOption: ", 0, 5))
         {
-        case 'A':
+        case 1:
+            n = inputInteger("\t\tEnter the size of the dynamic array: ", true);
+
+            populate(ds, n);
+
+            cout << "\t\t" << "Random elements have been populated into the array. \n\n";
             break;
-        case 'B':
+        case 2:
+            tempStr = inputString("\n\t\tEnter a string element: ", false);
+
+            ds.append(tempStr);
+
+            cout << "\t\t" << "Element \"" << tempStr << "\" has been added to the array. \n\n";
             break;
-        case 'C':
+        case 3:
+            if (ds.empty()) {
+                cout << "\t\t" << "The array is empty. \n\n";
+            }
+            else {
+                cout << "\t";
+                for (int i = 0; i < ds.size();i++)
+                    cout << ds[i] << " ";
+            }
+            cout << "\n";
             break;
-        case 'D':
+        case 4:
+            if (ds.empty()) {
+                cout << "\t\t" << "The array is empty. \n\n";
+            }
+            else {
+                choice = toupper(inputChar("\n\t\tChoose search type (S) Serial or (B) Binary: ", 'S', 'B'));
+
+                if (choice == 'S') {
+                    cout << "\t\t" << "Recursive serial search\n";
+                    cout << "\t\t" << string(65, (char)196) << "\n";
+                   
+                    tempStr = inputString("\t\tEnter a string element to search:",false);
+                    idx = serialStringSearch(ds,0, tempStr);
+                    if (idx != -1) {
+                        cout << "\t\t" << tempStr << " is found at index " << idx << " from the unsorted array. \n\n";
+                    }
+                    else
+                        cout << "\t\t\"" << tempStr << "\" is not found in the unsorted array. \n\n";
+                }
+                else {
+                    cout << "\t\t" << "Recursive binary search\n";
+                    cout << "\t\t" << string(65, (char)196) << "\n";
+                    tempStr = inputString("\t\tEnter a string element to search:", false);
+                    idx = binaryStringSearch(ds, 0,ds.size()-1, tempStr);
+                    if (idx != -1) {
+                        cout << "\t\t" << tempStr << " is found at index " << idx << " from the unsorted array. \n\n";
+                    }
+                    else
+                        cout << "\t\t\"" << tempStr << "\" is not found in the unsorted array. \n\n";
+                }
+            }
             break;
-        case 'E':
+        case 5:
+            if (ds.empty()) {
+                cout << "\t\t" << "The array is empty. \n\n";
+            }
+            else {
+                ds.clear();
+                cout << "\t\t" << "Elements from the array have been cleared/deleted.\n\n";
+            }
             break;
-        case '0': return;
+        case 0: return;
         default:
         {
             cout << "\n\tERROR: invalid input...";
         }break;
         }
+        cout << "\n\n\t";
         system("pause");
     } while (true);
 }
@@ -86,39 +184,40 @@ void option2() {
         system("cls");
 
 
-        cout << "\t2 > Searching sorted dynamic arrays\n";
+        cout << "\t1 > Searching sorted dynamic arrays\n";
         cout << "\t" << string(85, (char)205) << "\n";
-        cout << "\t\t" << "A > Read data file and store into the array\n";
-        cout << "\t\t" << "B > Add an element to the dynamic array\n";
-        cout << "\t\t" << "C > Display elements from the array\n";
-        cout << "\t\t" << "D > Search for an element from the array\n";
-        cout << "\t\t" << "E > Clear the array\n";
+        cout << "\t\t" << "1 > Read data file and store into the array\n";
+        cout << "\t\t" << "2 > Add an element to the dynamic array\n";
+        cout << "\t\t" << "3 > Display elements from the array\n";
+        cout << "\t\t" << "4 > Search for an element from the array\n";
+        cout << "\t\t" << "5 > Clear the array\n";
         cout << "\t" << string(85, (char)196) << "\n";
         cout << "\t\t" << "0 > return\n";
         cout << "\t" << string(85, (char)205) << "\n";
-
-        switch (toupper(inputChar("\t\tOption: ", static_cast<string>("abcdefgh0"))))
-        {
-        case 'A':
+        choice = inputChar("\t\tChoice:");
+        if (choice < '0' || choice > '5') {
+            while ((choice < '0') || (choice > '5')) {
+                choice = inputChar("\tERROR INVALID CHOICE. TRY AGAIN:");
+            }
+        }
+        switch (choice) {
+        case '1':
             break;
-        case 'B':
+        case '2':
             break;
-        case 'C':
+        case '3':
             break;
-        case 'D':
+        case '4':
             break;
-        case 'E':
+        case '5':
             break;
-        case 0: return;
-        default:
-        {
-            cout << "\n\tERROR: invalid input...";
-        }break;
+        case '0':
+            running = false;
+            break;
         }
         system("pause");
     } while (running);
 }
-
 char mainMenu() {
     char choice = 'A';
     cout << "\t" << "CMPR131 Chapter 12: Searches and hashing by Pedro Vargas (11/23/2025)\n";
